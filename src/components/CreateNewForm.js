5e8f54f544271b24897a9680a8ff395d78cfd336
Kristina Karvanova <kristaliska@gmail.com>
2022-10-06 20:54:22 +0300

    multiple_upload

--- src/components/CreateNewForm.js
+++ src/components/CreateNewForm.js
@@ -4,27 +4,43 @@ import React, { useState, useRef } from "react"
 function CreateNewForm(props) {
   const [city, setCity] = useState("")
   const [rooms, setRooms] = useState("")
-  const [file, setFile] = useState("")
-  const CreatePhotoField = useRef()
+  const [files, setFile] = useState([{
+    data: []
+}])
+  const tempArr = [];
+  const handleImageUpload = e => {
+    [...e.target.files].forEach(file => {
+        tempArr.push(
+            file
+        );
+    });
+    setFile(tempArr)
+};
+  
 
   async function submitHandler(e) {
     e.preventDefault()
     const data = new FormData()
-    data.append("photo", file)
+    
+    files.forEach(file => {
+        data.append("photos", file);
+    });
+    
     data.append("city", city)
     data.append("rooms", rooms)
     setCity("")
     setRooms("")
     setFile("")
-    CreatePhotoField.current.value = ""
+    
     const newPhoto = await Axios.post("/create-sublet", data, { headers: { "Content-Type": "multipart/form-data" } })
     props.setSublets(prev => prev.concat([newPhoto.data]))
+
   }
 
   return (
     <form className="p-3 bg-success bg-opacity-25 mb-5" onSubmit={submitHandler}>
       <div className="mb-2">
-        <input ref={CreatePhotoField} onChange={e => setFile(e.target.files[0])} type="file" className="form-control" />
+        <input onChange={handleImageUpload} name="file" type="file" className="form-control" multiple/>
       </div>
       <div className="mb-2">
         <input onChange={e => setCity(e.target.value)} value={city} type="text" className="form-control" placeholder="City" />
